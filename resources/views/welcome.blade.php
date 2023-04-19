c96a906d16396372b65149b90c39adc1307c6e28
Wajiha <wajiha.work@gmail.com>
2023-04-19 10:37:54 +0500

    layout page set

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+@extends('shared.LayoutPage')
+
+@section('title', 'Home Page')
+
+@section('content')
+
+   <!-- home page -->
+   <div class="container text-center bg-dark text-info mt-5 p-5">
+        <h1 class="display-6">Welcome to my Laravel CRUD application</h1>
+    </div>
+
+@endsection
